815f78c65215008aade0273b2639a02ed9040263
bigwheel <k.bigwheel+eng@gmail.com>
2015-09-19 22:45:10 +0900

    taskを調整

--- task/src/main/scala/TaskRunner.scala
+++ task/src/main/scala/TaskRunner.scala
@@ -14,16 +14,16 @@ object TaskRunner extends skinny.task.TaskLauncher {
   register("loadCards", _ => {
     val b = new Browser
     val doc = b.get("http://magic.wizards.com/ja/articles/archive/card-image-gallery/%E3%80%8E%E6%88%A6%E4%B9%B1%E3%81%AE%E3%82%BC%E3%83%B3%E3%83%87%E3%82%A3%E3%82%AB%E3%83%BC%E3%80%8F-2015-09-17")
-    println(doc)
+    // println(doc)
     val imageTags: Seq[Element] =
       doc >> elementList("div#content-detail-page-of-an-article > div > div > p > img")
     val nameAndImageUrls = imageTags.map(tag => (tag.attr("alt"), tag.attr("src")))
-    println(nameAndImageUrls)
+    // println(nameAndImageUrls)
 
     skinny.DBSettings.initialize()
-    //Card.deleteAll
-    //nameAndImageUrls.foreach(p => Card.createWithAttributes('name -> p._1, 'imageUrl -> p._2))
-    Card.findAll().foreach(c => println(c))
+    Card.deleteAll
+    nameAndImageUrls.foreach(p => Card.createWithAttributes('name -> p._1, 'imageUrl -> p._2))
+    // Card.findAll().foreach(c => println(c))
   })
 
 }
